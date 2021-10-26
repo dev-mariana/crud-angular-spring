e416320a96844a4ca5b3e54ffd20783c2e57b9ce
Mariana Bastos <maribastossrodrigues@gmail.com>
2021-10-25 22:50:52 -0300

    add spinner

--- crud-angular/src/app/shared/material/material.module.ts
+++ crud-angular/src/app/shared/material/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
     MatTableModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatProgressSpinnerModule
   ]
 })
 export class MaterialModule { }
